c13a533f386f2d7948d9c28a01e7a5eedbeb51da
Nahid Hasan <nahid675@diu.edu.bd>
2019-06-30 11:50:06 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountKit, AuthResponse } from 'ng2-account-kit';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'facebook-account-kit';
+
+  ngOnInit(): void {
+    AccountKit.init({
+      appId: '2229117677418303',
+      state: 'ssdfsdfsdfsdf',
+      version: 'v1.0',
+    });
+  }
+
+  login(): void {
+    AccountKit.login('PHONE', { countryCode: '+880', phoneNumber: '' }).then(
+      (response: AuthResponse) => console.log(response.code),
+      (error: any) => console.error(error)
+    );
+  }
 }
